0844a5382ccfedac698ebe04e3af6aee1b1e895a
Auston Stewart <auston@shedbuilt.net>
2018-01-12 02:31:31 -0500

    Adding the toolchain build mode.

--- build.sh
+++ build.sh
@@ -1,16 +1,28 @@
 #!/bin/bash
-# For FHS compliance, use /var/lib/hwclock for adjtime
-mkdir -pv "${SHED_FAKEROOT}/var/lib/hwclock"
-./configure ADJTIME_PATH=/var/lib/hwclock/adjtime \
-            --docdir=/usr/share/doc/util-linux-2.31.1 \
-            --disable-chfn-chsh \
-            --disable-login \
-            --disable-nologin \
-            --disable-su \
-            --disable-setpriv \
-            --disable-runuser \
-            --disable-pylibmount \
-            --disable-static \
-            --without-python || return 1
+case "$SHED_BUILDMODE" in
+    toolchain)
+        ./configure --prefix=/tools                \
+                    --without-python               \
+                    --disable-makeinstall-chown    \
+                    --without-systemdsystemunitdir \
+                    --without-ncurses              \
+                    PKG_CONFIG="" || return 1
+        ;;
+    *)
+        # For FHS compliance, use /var/lib/hwclock for adjtime
+        mkdir -pv "${SHED_FAKEROOT}/var/lib/hwclock"
+        ./configure ADJTIME_PATH=/var/lib/hwclock/adjtime \
+                --docdir=/usr/share/doc/util-linux-2.31.1 \
+                --disable-chfn-chsh \
+                --disable-login \
+                --disable-nologin \
+                --disable-su \
+                --disable-setpriv \
+                --disable-runuser \
+                --disable-pylibmount \
+                --disable-static \
+                --without-python || return 1
+        ;;
+esac
 make -j $SHED_NUMJOBS || return 1
 make DESTDIR="$SHED_FAKEROOT" install || return 1
