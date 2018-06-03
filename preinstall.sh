e42ed0a74784de9557a01b3747fb62da729ddace
Auston Stewart <auston@shedbuilt.net>
2018-06-02 18:31:43 -1000

    Eliminating unecessary symlink removal in bootstrap preinstall

--- preinstall.sh
+++ preinstall.sh
@@ -2,9 +2,5 @@
 declare -A SHED_PKG_LOCAL_OPTIONS=${SHED_PKG_OPTIONS_ASSOC}
 if [ -n "${SHED_PKG_LOCAL_OPTIONS[bootstrap]}" ]; then
     # Remove temporary symlinks created earlier in the bootstrap
-    for SHED_PKG_LOCAL_UTILLIB in blkid mount uuid
-    do
-        rm -v /usr/lib/lib${SHED_PKG_LOCAL_UTILLIB}.so*
-    done
     rm -vf /usr/include/{blkid,libmount,uuid}
 fi
